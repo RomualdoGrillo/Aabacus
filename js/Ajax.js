f735623eb4c9c746986dc435f416bec306f33238
RomualdoGrillo <rom.grillo@gmail.com>
2020-03-06 14:33:01 +0100

    preload JSON

--- js/Ajax.js
+++ js/Ajax.js
@@ -1,22 +1,3 @@
-function preload(myUrl) {
-	//preloadAjax('./Data/Preload/preload.mml')
-	  $.ajax({    
-			type: "GET",              
-			url: myUrl,
-			dataType: "text",
-			error: function (e) {
-				alert("AJAX/get fallita : verificare da Chrome");
-				console.log("Ajax/GET fallita : ", e);
-			},
-			success: function(response) {
-				//alert("lettura file " + myUrl + " tramite Ajax OK - risposta : " + response);
-				inject(response,$("#telaRole"))
-			}
-	  });
-}
-
-
-
 
 
 
--- js/preload.js
+++ js/preload.js
@@ -1,16 +1,47 @@
-function preload(myUrl) {
+function preloadAll(myUrl) {
+	//preloadAll('./Data/Preload/preload.json')
+	$.ajax({
+		type: "GET",
+		url: myUrl,
+		dataType: "text",
+		error: function(e) {
+			alert("AJAX/get fallita : verificare da Chrome");
+			console.log("Ajax/GET fallita : ", e);
+		},
+		success: function(response){injectAll(response,myUrl)}
+	});
+}
+function injectAll(response,rootUrl){
+	console.log(response);
+	let all = JSON.parse(response);
+	//console.log(all);
+	if(all.content.mml){
+		//console.log('string!')
+		inject(all.content.mml, $("#telaRole"))
+		}
+	else if(all.content){
+		preloadAjax(buildPath(rootUrl,all.content))
+	}
+}
+
+
+function preloadAjax(myUrl) {
 	//preloadAjax('./Data/Preload/preload.mml')
-	  $.ajax({    
-			type: "GET",              
+	if(myUrl){
+	//altrimenti un url vuoto verrebbe interpretato come path relativo,
+	//col risultato di caricare index.html
+		$.ajax({
+			type: "GET",
 			url: myUrl,
 			dataType: "text",
-			error: function (e) {
+			error: function(e) {
 				alert("AJAX/get fallita : verificare da Chrome");
 				console.log("Ajax/GET fallita : ", e);
 			},
 			success: function(response) {
-				//alert("lettura file " + myUrl + " tramite Ajax OK - risposta : " + response);
-				inject(response,$("#telaRole"))
+			//alert("lettura file " + myUrl + " tramite Ajax OK - risposta : " + response);
+			inject(response, $("#telaRole"))
 			}
-	  });
+		});
+	}
 }
