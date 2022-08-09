73af0854a90458f5872dfdfc7fc12d1b0c04665f
RomualdoGrillo <rom.grillo@gmail.com>
2022-08-09 11:47:06 +0200

    Animalia and Set Display

---
+++ js/SetDisplay.js
@@ -0,0 +1,16 @@
+function columnsSize($orNode){
+    $.MNODE_getChildren();
+    //offsetHeight
+}
+
+function exploreTree($startNode){
+    //get children
+    //group subfolders by criteria: #main:[a,b,c]  #feeding:['herbivore'',carnivore']
+    //if(number of criteria>1){
+        //present user with choice}
+    //else{ } 
+}
+
+function treeNextLevel(){
+    
+}
