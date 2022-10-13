6336dcb0886593f6479e3b16a10020c037f28236
RomualdoGrillo <rom.grillo@gmail.com>
2022-10-14 00:45:43 +0200

    SaveASHtml ok load not working

--- js/inflatedeflate.js
+++ js/inflatedeflate.js
@@ -25,9 +25,10 @@ function MNODEcreateMathmlString($startNodes,describeDataType, neglectRootSign) 
 
 function createConvertedTree(startNodeOrMML, from_to, neglectRootSign,toBeImported) {
 	var $containerForClone = $('<div></div>')
-	//$('#testSpan').html("") ;var $containerForClone = $('#testSpan');// debug
+	//$('#canvasRole').html("") ;var $containerForClone = $('#canvasRole');// debug
 	//var $thisClone = $($.parseXML(startNode).firstElementChild)
-	let $startNodeOrMML = $(startNodeOrMML)
+	let $startNodeOrMML = $(startNodeOrMML)// problema con file misti mml con html vedi Quaderno Aprile Giugno Agosto settembre ottobre 2022
+	//let $startNodeOrMML=$($.parseXML(startNodeOrMML)).children(':first')
 	//try to rebuild the here??
 	if (from_to === "aab_mml" || from_to === "aab_mmlWithType") {
 		let $thisClone = $startNodeOrMML.clone()
@@ -44,7 +45,7 @@ function createConvertedTree(startNodeOrMML, from_to, neglectRootSign,toBeImport
 
 		//signsAsClassesSubtree($thisClone,"SignsAsClasses_to_MinusOp")// converti in modo che il segno meno sia una operazione applicata al nodo
 		//sostituisci tutti i nodi MNODE excluding prototypes
-		$thisClone.parent().find('[data-atom]').not('[data-proto]').each(function(i, node) {
+		$thisClone.parent().find('[data-atom]').not('[data-proto]').not('.saveAsHtml').each(function(i, node) {
 			if (i == 0) {
 				ReplaceOneMNODE(node, from_to, neglectRootSign);
 			} else {
@@ -79,8 +80,6 @@ function ReplaceOneMNODE(node, from_to, neglectSign) {
 	//node is HTML node
 	let $node = $(node);
 	var $newNode
-	var isMinimized
-	var isMedium
 	var originalData
 	var title
 	if (from_to === "aab_mml" || from_to === "aab_mmlWithType") {
@@ -150,8 +149,6 @@ function ReplaceOneMNODE(node, from_to, neglectSign) {
 		if (atom === "math") {
 			$newNode = $node.children()
 			//unwrap "math"
-		} else if (atom === "saveAsHtml") {//gestire qui 
-		//non sostituire e non fare nulla
 		} else {
 			var $children = $node.children().not(':first')
 			//search for prototype
