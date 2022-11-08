77f4877ca2f96883c6bc6596930be3cb9a015e7e
RomualdoGrillo <rom.grillo@gmail.com>
2022-11-08 10:22:38 +0100

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -3,7 +3,7 @@
 
 
 var clickSound = new Audio('./sounds/click.mp3');
-var blipSound = new Audio('./sounds/blip.wav');//blimp.mp3 is too long: two seconds
+var blipSound = new Audio('./sounds/Blip.wav');//blimp.mp3 is too long: two seconds
 var deleteSound = new Audio('./sounds/delete.mp3');
 var disconnectSound = new Audio('./sounds/disconnect.mp3');
 var victorySound = new Audio('./sounds/Magic-Spell.mp3');
