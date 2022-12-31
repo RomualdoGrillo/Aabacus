9118048c39a41ada111e2552c9d20626482681c9
RomualdoGrillo <rom.grillo@gmail.com>
2022-12-31 14:03:18 +0100

    highlight connected

--- js/DnD.js
+++ js/DnD.js
@@ -23,7 +23,7 @@ function MakeSortableAndInjectMouseDown(event) {
 	if($atomTarget.attr('data-atom')=='ci'){
 		$identifierSpan($atomTarget).addClass('mu_FocusSpan');
 	}
-	highlightOccurrences($atomTarget,'link');
+	highlightOccurrences($atomTarget,'mu_connected');
 
 	//**** highlight DOWNSTREAM 
 	// add class mu_Downstream1, downstrwam2, downstream3, downstreamuFurther
